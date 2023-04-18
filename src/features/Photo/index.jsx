5e4a7c0efa81d74002b0fd56346accb2a4c0eba7
Nhat Le <symbol057vn@gmail.com>
2023-04-18 11:38:59 +0700

    set files + add reactstrap

---
+++ src/features/Photo/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Photo.propTypes = {
+
+};
+
+function Photo(props) {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default Photo;
